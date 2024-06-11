c91570582a5dd1800cd252dfe26ce9a248f654ab
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2024-06-11 08:51:30 -0600

    First commit. Adding so app will also be ony my github

---
+++ RestaurantManagementSystem/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MySqlConnector;
+using System;
+
+namespace RestaurantManagementSystem
+
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Opens the respective pages based on the button clicked
+
+        private async void OnReservationButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Reservation_input());
+        }
+
+        private async void OnGoToOrderButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OrderMain());
+        }
+
+        private async void OnAddCustomerButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ManageCustomers());
+        }
+
+        private async void OnOrderNowButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OrderNow());
+        }
+
+        private async void OrderHistoryButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OrderHistory());
+        }
+
+    }
+
+
+
+
+    
+
+
+}
---
+++ RestaurantManagementSystem/Reservation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem
+{
+    public class Reservation
+    {
+        private int _bookingNumber;
+        public int BookingNumber
+        {
+            get => _bookingNumber;
+            set => _bookingNumber = value;
+        }
+
+        private DateTime _schedule;
+        public DateTime Schedule
+        {
+            get => _schedule;
+            set => _schedule = value;
+        }
+
+        private Customer _customer;
+        public Customer Customer
+        {
+            get => _customer;
+            set => _customer = value;
+        }
+
+        private string _boothName;
+        public string BoothName
+        {
+            get => _boothName;
+            set => _boothName = value;
+        }
+        public Reservation(int parambookingNumber, DateTime paramSchedule, Customer paramCustomer, string paramBoothName)
+        {
+            BookingNumber = parambookingNumber;
+            Schedule = paramSchedule;
+            Customer = paramCustomer;
+            BoothName = paramBoothName;
+        }
+
+        public Reservation() { }
+
+        public string FullDetails => $"{_schedule.ToString("M/d/yyyy")} {_boothName} at {_schedule.Hour}:00 for: {_customer.Name}";
+
+        public override string ToString()
+        {
+            return $"{_schedule.ToString("M/d/yyyy")} {_boothName} at {_schedule.Hour}:00 for: {_customer.Name}";
+        }
+    }
+}
